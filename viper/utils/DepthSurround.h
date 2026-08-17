6cca7eb72b86f9fa8c92bf7f0a1949d1bd225f7a
Dung Nguyen <158302785+DungxND@users.noreply.github.com>
2026-08-17 22:42:48 +0700

    refactor(cpp23): modernize ColorfulMusic cluster with noexcept, std::array, and member init

--- viper/utils/DepthSurround.h
+++ viper/utils/DepthSurround.h
@@ -2,27 +2,26 @@
 
 #include "Biquad.h"
 #include "TimeConstDelay.h"
+#include <array>
+#include <cstdint>
 
 class DepthSurround {
 public:
     DepthSurround();
 
-    void Process(float *samples, uint32_t size);
+    void Process(float* samples, uint32_t size);
 
     void SetStrength(uint32_t value);
     void SetSamplingRate(uint32_t sampling_rate);
-
     void RefreshStrength(uint32_t strength);
 
 private:
-    bool enable_;
-    bool strength_at_least500_;
+    bool     enable_              = false;
+    bool     strength_at_least500_ = false;
+    uint32_t strength_            = 0;
+    float    gain_                = 0.0f;
 
-    uint32_t strength_;
-
-    float gain_;
-    float prev_[2] = {};
-
-    TimeConstDelay time_const_delay_[2];
-    Biquad highpass_;
+    std::array<float, 2>         prev_{};
+    std::array<TimeConstDelay, 2> time_const_delay_;
+    Biquad                        highpass_;
 };
--- viper/utils/Stereo3DSurround.cpp
+++ viper/utils/Stereo3DSurround.cpp
@@ -1,65 +1,47 @@
 #include "Stereo3DSurround.h"
 
-Stereo3DSurround::Stereo3DSurround() :
-    stereo_widen_(0.0f),
-    middle_image_(1.0f),
-    coeff_left_(0.5f),
-    coeff_right_(0.5f) {}
+Stereo3DSurround::Stereo3DSurround() noexcept = default;
 
-void Stereo3DSurround::Process(float *sample, const uint32_t size) const {
+void Stereo3DSurround::Process(float* const samples, const uint32_t size) const noexcept {
     if (size == 0) return;
 
-    const uint32_t pairs = size / 2;
+    const uint32_t pairs     = size / 2;
     const uint32_t remainder = size % 2;
 
-    if (pairs > 0) {
-        for (uint32_t i = 0; i < pairs; i++) {
-            const float a = coeff_left_ * (sample[4 * i] + sample[4 * i + 1]);
-            const float b = coeff_right_ * (sample[4 * i + 1] - sample[4 * i]);
-            const float c = coeff_left_ * (sample[4 * i + 2] + sample[4 * i + 3]);
-            const float d = coeff_right_ * (sample[4 * i + 3] - sample[4 * i + 2]);
+    for (uint32_t i = 0; i < pairs; i++) {
+        const float a = coeff_left_  * (samples[4 * i]     + samples[4 * i + 1]);
+        const float b = coeff_right_ * (samples[4 * i + 1] - samples[4 * i]);
+        const float c = coeff_left_  * (samples[4 * i + 2] + samples[4 * i + 3]);
+        const float d = coeff_right_ * (samples[4 * i + 3] - samples[4 * i + 2]);
 
-            sample[4 * i] = a - b;
-            sample[4 * i + 1] = a + b;
-            sample[4 * i + 2] = c - d;
-            sample[4 * i + 3] = c + d;
-        }
+        samples[4 * i]     = a - b;
+        samples[4 * i + 1] = a + b;
+        samples[4 * i + 2] = c - d;
+        samples[4 * i + 3] = c + d;
     }
 
-    if (remainder > 0) {
-        for (uint32_t i = 4 * pairs; i < 2 * size; i += 2) {
-            const float a = sample[i];
-            const float b = sample[i + 1];
-            const float c = coeff_left_ * (a + b);
-            const float d = coeff_right_ * (b - a);
-
-            sample[i] = c - d;
-            sample[i + 1] = c + d;
-        }
+    for (uint32_t i = 4 * pairs; remainder > 0 && i < 2 * size; i += 2) {
+        const float a = samples[i];
+        const float b = samples[i + 1];
+        samples[i]     = coeff_left_  * (a + b) - coeff_right_ * (b - a);
+        samples[i + 1] = coeff_left_  * (a + b) + coeff_right_ * (b - a);
     }
 }
 
-void Stereo3DSurround::SetMiddleImage(const float value) {
+void Stereo3DSurround::SetMiddleImage(const float value) noexcept {
     middle_image_ = value;
     ConfigureVariables();
 }
 
-void Stereo3DSurround::SetStereoWiden(const float value) {
+void Stereo3DSurround::SetStereoWiden(const float value) noexcept {
     stereo_widen_ = value;
     ConfigureVariables();
 }
 
-void Stereo3DSurround::ConfigureVariables() {
+void Stereo3DSurround::ConfigureVariables() noexcept {
     const float tmp = stereo_widen_ + 1.0f;
-
-    const float x = tmp + 1.0f;
-    float y;
-    if (x < 2.0f) {
-        y = 0.5f;
-    } else {
-        y = 1.0f / x;
-    }
-
-    coeff_left_ = middle_image_ * y;
+    const float x   = tmp + 1.0f;
+    const float y   = (x < 2.0f) ? 0.5f : (1.0f / x);
+    coeff_left_  = middle_image_ * y;
     coeff_right_ = tmp * y;
 }
