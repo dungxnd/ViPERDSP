4c71c283a458729701b178c97d569fdf4f2a32e8
Dung Nguyen <158302785+DungxND@users.noreply.github.com>
2026-08-19 21:58:00 +0700

    refactor(cpp23): modernize MultibandCompressor with C++23 idioms and noexcept

--- viper/effects/MultibandCompressor.h
+++ viper/effects/MultibandCompressor.h
@@ -3,48 +3,48 @@
 #include "../utils/MultiBiquad.h"
 #include "FETCompressor.h"
 #include <array>
+#include <cstdint>
 #include <vector>
 
 class MultibandCompressor {
 public:
-    static constexpr uint32_t kMaxBands = 5;
+    static constexpr uint32_t kMaxBands      = 5;
     static constexpr uint32_t kMaxCrossovers = 4;
 
     MultibandCompressor();
 
-    void Process(float *samples, uint32_t size);
-    void Reset();
+    void Process(float* samples, uint32_t size) noexcept;
+    void Reset() noexcept;
 
-    void SetEnable(bool enable);
+    void SetEnable(bool enable) noexcept;
     void SetBandCount(uint32_t count);
-    void SetCrossoverFrequency(uint32_t index, float frequency);
-    void SetSamplingRate(uint32_t sampling_rate);
+    void SetCrossoverFrequency(uint32_t index, float frequency) noexcept;
+    void SetSamplingRate(uint32_t sampling_rate) noexcept;
 
-    void SetBandEnable(uint32_t band, bool enable);
-    void SetBandThreshold(uint32_t band, float value);
-    void SetBandRatio(uint32_t band, float value);
-    void SetBandKnee(uint32_t band, float value);
-    void SetBandKneeAuto(uint32_t band, bool enable);
-    void SetBandGain(uint32_t band, float value);
-    void SetBandGainAuto(uint32_t band, bool enable);
-    void SetBandAttack(uint32_t band, float value);
-    void SetBandAttackAuto(uint32_t band, bool enable);
-    void SetBandRelease(uint32_t band, float value);
-    void SetBandReleaseAuto(uint32_t band, bool enable);
-    void SetBandKneeMulti(uint32_t band, float value);
-    void SetBandMaxAttack(uint32_t band, float value);
-    void SetBandMaxRelease(uint32_t band, float value);
-    void SetBandCrest(uint32_t band, float value);
-    void SetBandAdapt(uint32_t band, float value);
-    void SetBandNoClip(uint32_t band, bool enable);
+    void SetBandEnable(uint32_t band, bool enable) noexcept;
+    void SetBandThreshold(uint32_t band, float value) noexcept;
+    void SetBandRatio(uint32_t band, float value) noexcept;
+    void SetBandKnee(uint32_t band, float value) noexcept;
+    void SetBandKneeAuto(uint32_t band, bool enable) noexcept;
+    void SetBandGain(uint32_t band, float value) noexcept;
+    void SetBandGainAuto(uint32_t band, bool enable) noexcept;
+    void SetBandAttack(uint32_t band, float value) noexcept;
+    void SetBandAttackAuto(uint32_t band, bool enable) noexcept;
+    void SetBandRelease(uint32_t band, float value) noexcept;
+    void SetBandReleaseAuto(uint32_t band, bool enable) noexcept;
+    void SetBandKneeMulti(uint32_t band, float value) noexcept;
+    void SetBandMaxAttack(uint32_t band, float value) noexcept;
+    void SetBandMaxRelease(uint32_t band, float value) noexcept;
+    void SetBandCrest(uint32_t band, float value) noexcept;
+    void SetBandAdapt(uint32_t band, float value) noexcept;
+    void SetBandNoClip(uint32_t band, bool enable) noexcept;
 
 private:
-    bool enable_;
+    bool     enable_{false};
+    uint32_t sampling_rate_{44100};
+    uint32_t band_count_{3};
 
-    uint32_t sampling_rate_;
-    uint32_t band_count_;
-
-    std::array<float, kMaxCrossovers> crossover_freqs_;
+    std::array<float, kMaxCrossovers>       crossover_freqs_{200.0f, 4000.0f, 0.0f, 0.0f};
 
     std::array<MultiBiquad, kMaxCrossovers> lowpass_la_;
     std::array<MultiBiquad, kMaxCrossovers> lowpass_lb_;
@@ -55,9 +55,8 @@ private:
     std::array<MultiBiquad, kMaxCrossovers> highpass_ra_;
     std::array<MultiBiquad, kMaxCrossovers> highpass_rb_;
 
-    std::array<FETCompressor, kMaxBands> compressors_;
+    std::array<FETCompressor, kMaxBands>          compressors_;
+    std::array<std::vector<float>, kMaxBands>     band_buffers_;
 
-    std::array<std::vector<float>, kMaxBands> band_buffers_;
-
-    void ConfigureCrossovers();
+    void ConfigureCrossovers() noexcept;
 };
